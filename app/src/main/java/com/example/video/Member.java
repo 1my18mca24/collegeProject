9ce42cc248d15db9bee5f41f167a90357bf5bbb5
SNEHA <1my18mca24@gmail.com>
2020-11-22 21:17:48 +0530

    Fetching videos from firebase to recyclerview

---
+++ app/src/main/java/com/example/video/Member.java
@@ -0,0 +1,23 @@
+package com.example.video;
+
+public class Member {
+
+    String name,videoUrl;
+    public Member(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getvideoUrl() {
+        return videoUrl;
+    }
+
+    public void setvideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
